834cfb131d316dfd32775e2d01a3537c61b238f0
Meriem OUNI <elounimeriem@gmail.com>
2021-02-25 20:08:51 +0100

    first commit

---
+++ src/components/MoviesData.js
@@ -0,0 +1,23 @@
+import movie1 from '../image/joker.jpg';
+import movie2 from '../image/avatar.jpg';
+import movie3 from '../image/fivefeetapart.jpg';
+import movie4 from '../image/thefaultinourstars.jpg';
+import movie5 from '../image/godfather.jpg';
+import movie6 from '../image/thevow.jpg';
+import movie7 from '../image/amadeus.jpg';
+import movie8 from '../image/forrestgump.jpg';
+
+
+
+const moviesData = [
+    { id: Math.random(), title: "Joker", img: movie1, rating: 5 },
+    { id: Math.random(), title: "Avatar", img: movie2, rating: 4 },
+    { id: Math.random(), title: "Five Feet Apart", img: movie3, rating: 3 },
+    { id: Math.random(), title: "The Fault In Our Stars", img: movie4, rating: 2 },
+    { id: Math.random(), title: "The Godfather", img: movie5, rating: 5 },
+    { id: Math.random(), title: "The Vow", img: movie6, rating: 4 },
+    { id: Math.random(), title: "Amadeus", img: movie7, rating: 3 },
+    { id: Math.random(), title: "Forrest Gump", img: movie8, rating: 3 },
+]
+
+export default moviesData
